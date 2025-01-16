806a5a301c70ec821218f8b9cc5cae61a9cd0577
sajedakhasawneh <sajedakhasawneh5@gmail.com>
2025-01-16 16:18:35 +0300

    Add project files.

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //string word;
+            //word = Console.ReadLine();
+            //Console.WriteLine(word);
+
+
+
+
+            //task2
+            double num = 1.2;
+            string  = "";
+        }
+    }
+}
